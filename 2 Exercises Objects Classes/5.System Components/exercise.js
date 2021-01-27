bef3c3a3c7aa7191dd8d65508d45c694f5c49ef8
OpKata <60965798+OpKata@users.noreply.github.com>
2021-01-27 11:19:19 +0200

    veri helpful hintl for  sort function in JS

---
+++ 2 Exercises Objects Classes/5.System Components/exercise.js
@@ -0,0 +1,48 @@
+function solve (input){
+    let result = {};
+
+    input.forEach(line => {
+       let[systemName, component, subCompoment] = line.split(" | ");
+       
+       if(!result[systemName]){
+        result[systemName] = {};
+       }
+       
+
+       if(!result[systemName][component]){
+        result[systemName][component] = [];
+    }
+    result[systemName][component].push(subCompoment);
+    })
+    Object.entries(result).sort((curr,next) => {
+        return Object.entries(next[1]).length - Object.entries(curr[1]).length || curr[0].localeCompare(next[0]);
+    }).forEach(([system, component]) => {
+        console.log(system);
+        Object.entries(component).sort((curr,next) => {
+            return next[1].length - curr[1].next;
+        }).forEach(([component, subCompoment]) => {
+            console.log(`|||${component}`)
+            subCompoment.forEach( e => {
+               console.log(`||||||${e}`);     
+            });
+        })
+    })
+    
+
+}
+
+
+solve(['SULS | Main Site | Home Page',
+'SULS | Main Site | Login Page',
+'SULS | Main Site | Register Page',
+'SULS | Judge Site | Login Page',
+'SULS | Judge Site | Submittion Page',
+'Lambda | CoreA | A23',
+'SULS | Digital Site | Login Page',
+'Lambda | CoreB | B24',
+'Lambda | CoreA | A24',
+'Lambda | CoreA | A25',
+'Lambda | CoreC | C4',
+'Indice | Session | Default Storage',
+'Indice | Session | Default Security']
+)
